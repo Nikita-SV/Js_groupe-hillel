d91015149941b428c74dd1767a4a9be4a0efa274
Mykyta <snikita0298@gmail.com>
2023-11-24 22:37:51 +0300

    Home work

--- js/index.js
+++ js/index.js
@@ -1,1 +1,48 @@
-console.log('Hello world');
+// ------------ 1 Task ------------
+/*
+let firstName = prompt('What is your name?');
+if(Number(firstName)){
+    alert(`Write you are Name`)
+}else if(String(firstName)){
+    alert(`Hello, ${firstName}! How are you?`)
+}else{
+    alert(`Well come!`)
+} */    
+
+// ------------ 2 Task ------------
+
+/*
+let calkNumOne = prompt(`Введите первое число`);
+let calkOperator = prompt(`Введите оператор - + * /`);
+let calkNumTwo = prompt(`Введите второе число`);
+
+let calcResult 
+
+if (calkOperator === "/"){
+    calcResult = calkNumOne / calkNumTwo
+}else if(calkOperator === "*"){
+    calcResult = calkNumOne * calkNumTwo
+}else if(calkOperator === "+"){
+    calcResult = Number(calkNumOne) + Number(calkNumTwo)
+}else if(calkOperator === "-"){
+    calcResult = calkNumOne - calkNumTwo
+}
+calcResult = isNaN(calcResult) ? alert(`Введены неверные значения`) : alert(calcResult);
+*/
+
+// ------------ 3 Task ------------
+
+/*
+let numOne = prompt("Введите число 'a'");
+let numTwo = prompt("Введите число 'b'");
+
+if (numOne === numTwo){
+    alert('true')
+}else{
+    alert('false')
+}
+*/
+
+// ------------ 4 Task ------------
+
+/* */
