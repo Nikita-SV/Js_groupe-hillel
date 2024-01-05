5b1ee19da8d9e3621ab06a1c85510006655774f0
Mykyta <snikita0298@gmail.com>
2024-01-05 17:26:56 +0200

    domashka-7

---
+++ js/Domashka-7.js
@@ -0,0 +1,15 @@
+const array = [1, '2', 5, 3, '5', 4, 5, 6, '7'];
+function removeElement(arrayInput, item){
+    let j = 0
+    for (removeItem of arrayInput){
+        if (removeItem === item){
+            arrayInput.splice(j , 1);
+        }else{
+            j++;
+        }
+    }
+}
+
+removeElement(array, 5);
+console.log(array)
+// Результат: [1, '2', 3, '5', 4, 6, '7']
