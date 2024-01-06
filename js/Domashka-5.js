03204d88a9b906b6f71e07d6bff37ec2c949026d
Mykyta <snikita0298@gmail.com>
2024-01-06 02:31:10 +0200

    domashka 8

--- js/Domashka-5.js
+++ js/Domashka-5.js
@@ -1,20 +1,20 @@
 
-const courseDollar = 40;
-for(i = 10; i <= 100; i+=10){
-    let result = i * courseDollar
-    console.log(`${i}$ ровняется ${result} UAH`)
-}
+// const courseDollar = 40;
+// for(let i = 10; i <= 100; i+=10){
+//     const result = i * courseDollar
+//     console.log(`${i}$ ровняется ${result} UAH`)
+// }
 
 const inputNum = prompt('Введите число');
 
-if (!inputNum?.trim() || isNaN(inputNum) || !+inputNum || +inputNum < 0 || !Number.isInteger(+prompt)){
+if (!inputNum?.trim() || isNaN(inputNum) || !Number.isInteger(+inputNum)){
     alert(`Не валидные данные`)
-}else if(inputNum === 1){
+}else if(+inputNum === 1){
     alert(`${inputNum} является простым`)
 }else{
-    let inputNumConvert = +inputNum
+    const inputNumConvert = +inputNum
     let counter = 0
-    for(i=2; i<inputNumConvert; i++){
+    for(let i=2; i<inputNumConvert; i++){
         if (Number.isInteger(inputNumConvert / i)){
             counter++
         }
@@ -23,22 +23,22 @@ if (!inputNum?.trim() || isNaN(inputNum) || !+inputNum || +inputNum < 0 || !Numb
 
 }
 
-const inputNumForTree = prompt('Введите число');
-let numTree = 3;
-let resultNumTree = 0;
-if (!inputNumForTree?.trim() || isNaN(inputNumForTree) || !+inputNumForTree || +inputNumForTree < 0 || !Number.isInteger(+inputNumForTree)){
-    alert(`Не валидные данные`)
-}else{
-    let inputNumForTreeConvert = +inputNumForTree
-    for(i=1;resultNumTree < inputNumForTreeConvert; i++){
-        resultNumTree = Math.pow(numTree,i);
-        if (resultNumTree === inputNumForTreeConvert){
-            alert(`${inputNumForTreeConvert} возможно получить путем возведение числа 3`)
-        }
-    }
-    if(resultNumTree > inputNumForTreeConvert || resultNumTree !== inputNumForTreeConvert){
-        alert(`${inputNumForTreeConvert} невозможно получить путем возведение числа 3`)
-    }
-}
+// const inputNumForTree = prompt('Введите число');
+// const numTree = 3;
+// let resultNumTree = 0;
+// if (!inputNumForTree?.trim() || isNaN(inputNumForTree) /*|| !+inputNumForTree*/){
+//     alert(`Не валидные данные`)
+// }else{
+//     const inputNumForTreeConvert = +inputNumForTree
+//     for(let i=0; resultNumTree < inputNumForTreeConvert; i++){
+//         resultNumTree = Math.pow(numTree, i);
+//         if (resultNumTree === inputNumForTreeConvert){
+//             alert(`${inputNumForTreeConvert} возможно получить путем возведение числа 3`)
+//         }
+//     }
+//     if(resultNumTree > inputNumForTreeConvert || resultNumTree !== inputNumForTreeConvert){
+//         alert(`${inputNumForTreeConvert} невозможно получить путем возведение числа 3`)
+//     }
+// }
 
 
---
+++ js/Domashka-8.js
@@ -0,0 +1,24 @@
+const inputArr = prompt('Enter values separated by " , " :')
+
+function showSumNum(arrWhithNum){
+    let sumNum = 0;
+    if(arrWhithNum === null){
+        alert('Ops... Okey.')
+    }
+
+    else if(!arrWhithNum){
+        alert("Nothing entered")
+    }
+    
+    else{
+        const newArr = arrWhithNum.split(",");
+        for(itemArr of newArr){
+            if(!isNaN(+itemArr)){
+                sumNum += +itemArr
+            }
+        }
+        return alert(sumNum)
+    }
+}
+
+showSumNum(inputArr)
