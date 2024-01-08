2df3972dc61ee791ee56e6d7f3d20a656e580da4
Mykyta <snikita0298@gmail.com>
2024-01-08 12:37:23 +0200

    edit domashka 10 #2

--- js/Domashka-10.js
+++ js/Domashka-10.js
@@ -1,9 +1,13 @@
 const baseNum = prompt('Введите число которое нужно возвести в степень:') 
 const degree = prompt('Введите степень:') 
-let result = baseNum;
-let counter = 0;
+
 
 function checkValue (checkBase, checkDegree){
+    const numBase = +checkBase
+    const numDegree = +checkDegree
+    let result = numBase;
+    let counter = 0;
+    
     if (checkBase === null || checkDegree === null) return alert('Okey... u cancel')
 
     if(!checkBase?.trim() ||!checkDegree?.trim() || isNaN(checkBase) || isNaN(checkDegree)){
@@ -23,26 +27,27 @@ function checkValue (checkBase, checkDegree){
     }
 
     else{
-        const numBase = +checkBase
-        const numDegree = +checkDegree
+        
+        
+        return pow(numBase,numDegree)
+    }
 
-        pow(numBase,numDegree)
+    function pow(base, degree){
+        if (degree > 0){
+            counter++
+            if(counter === degree){
+                return alert(`Результат возведения в степень: ${result}`)
+            }
+        }else if(degree < 0){
+            counter--
+            if(counter === degree){
+                return alert(`Результат возведения в степень: ${1 / result}`)
+            }
+        }
+        result *= base
+        pow(base, degree)
     }
 }
-function pow(base, degree){
-    if (degree > 0){
-        counter++
-        if(counter === degree){
-            return alert(`Результат возведения в степень: ${result}`)
-        }
-    }else if(degree < 0){
-        counter--
-        if(counter === degree){
-            return alert(`Результат возведения в степень: ${1 / result}`)
-        }
-    }
-    result *= base
-    pow(base, degree)
-}
+
 
 checkValue(baseNum, degree);
